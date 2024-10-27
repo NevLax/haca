35702fb509bd48e79ef193373c12d2308ca47dfb
NevLax <86668758+NevLax@users.noreply.github.com>
2024-10-27 12:25:02 +0400

    Add files via upload

--- pap.py
+++ pap.py
@@ -3,19 +3,33 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
+
+#----------------------------------
+def DoSomething(fileImage):
+    pass                            #main logic
+                                    #return json
+#-----------------------------------
+
+
 @app.route('/process_image', methods=['POST'])
 def main_upload():
         file = request.files['file']
         if not file:
                 return jsonify({'message': 'No file'}), 400
 
-    #work with image
+    
+        result = DoSomething(file)
+
+
+        #work with image
+        # return result
+        
         return jsonify({    #return json data
         'message': 'data'
                 }), 200
 
 SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.yaml'
+API_URL = '/swagger.yaml'
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
@@ -26,4 +40,5 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
         app.run()
+
 
